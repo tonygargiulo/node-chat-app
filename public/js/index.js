953d79adf61372a7dbc91dfdf3a5131028e01a5c
tonygargiulo <tony@gargiulo.com>
2017-09-18 19:10:34 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log("Connected to Server");
 });
@@ -14,6 +31,8 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -26,6 +45,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('disconnect', function () {
